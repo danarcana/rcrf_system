fee339fdbde6eb9be54b360d51f68d8bf4173b04
dan dan <dan_arcana@yahoo.com>
2021-12-27 08:17:24 +0200

    ZIgbee Minimal initial project

---
+++ ZigbeeMinimal_Learn/config/pin_config.h
@@ -0,0 +1,115 @@
+#ifndef PIN_CONFIG_H
+#define PIN_CONFIG_H
+
+// $[ACMP0]
+// [ACMP0]$
+
+// $[ACMP1]
+// [ACMP1]$
+
+// $[ADC0]
+// [ADC0]$
+
+// $[CMU]
+// [CMU]$
+
+// $[DBG]
+// DBG SWV on PF2
+#define DBG_SWV_PORT                             gpioPortF
+#define DBG_SWV_PIN                              2
+#define DBG_SWV_LOC                              0
+
+// [DBG]$
+
+// $[ETM]
+// [ETM]$
+
+// $[PTI]
+// [PTI]$
+
+// $[GPIO]
+// [GPIO]$
+
+// $[I2C0]
+// [I2C0]$
+
+// $[I2C1]
+// [I2C1]$
+
+// $[LESENSE]
+// [LESENSE]$
+
+// $[LETIMER0]
+// [LETIMER0]$
+
+// $[LEUART0]
+// [LEUART0]$
+
+// $[LFXO]
+// [LFXO]$
+
+// $[MODEM]
+// [MODEM]$
+
+// $[PCNT0]
+// [PCNT0]$
+
+// $[PRS.CH0]
+// [PRS.CH0]$
+
+// $[PRS.CH1]
+// [PRS.CH1]$
+
+// $[PRS.CH2]
+// [PRS.CH2]$
+
+// $[PRS.CH3]
+// [PRS.CH3]$
+
+// $[PRS.CH4]
+// [PRS.CH4]$
+
+// $[PRS.CH5]
+// [PRS.CH5]$
+
+// $[PRS.CH6]
+// [PRS.CH6]$
+
+// $[PRS.CH7]
+// [PRS.CH7]$
+
+// $[PRS.CH8]
+// [PRS.CH8]$
+
+// $[PRS.CH9]
+// [PRS.CH9]$
+
+// $[PRS.CH10]
+// [PRS.CH10]$
+
+// $[PRS.CH11]
+// [PRS.CH11]$
+
+// $[TIMER0]
+// [TIMER0]$
+
+// $[TIMER1]
+// [TIMER1]$
+
+// $[USART0]
+// [USART0]$
+
+// $[USART1]
+// [USART1]$
+
+// $[USART2]
+// [USART2]$
+
+// $[VDAC0]
+// [VDAC0]$
+
+// $[WTIMER0]
+// [WTIMER0]$
+
+#endif // PIN_CONFIG_H
+
